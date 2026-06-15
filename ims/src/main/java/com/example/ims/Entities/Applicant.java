bc88ad91d75032d1e9ddd21d447f413dd16602f6
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 10:00:14 +0400

    add lombok annotation

--- ims/src/main/java/com/example/ims/Entities/Applicant.java
+++ ims/src/main/java/com/example/ims/Entities/Applicant.java
@@ -1,10 +1,16 @@
 package com.example.ims.Entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Applicant extends Person{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- ims/src/main/java/com/example/ims/Entities/AsylumSeeker.java
+++ ims/src/main/java/com/example/ims/Entities/AsylumSeeker.java
@@ -1,8 +1,14 @@
 package com.example.ims.Entities;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class AsylumSeeker extends Applicant {
     private String countryOfOrigin;
     private String sponsorOrganization;
