54f993d3a6057ce63312f9fb2512e43c95a4690e
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 09:56:54 +0400

    add fields

--- ims/src/main/java/com/example/ims/Entities/AsylumSeeker.java
+++ ims/src/main/java/com/example/ims/Entities/AsylumSeeker.java
@@ -1,4 +1,10 @@
 package com.example.ims.Entities;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class AsylumSeeker  extends Applicant{
+    private String countryOfOrigin;
+    private String sponsorOrganization;
+
 }
