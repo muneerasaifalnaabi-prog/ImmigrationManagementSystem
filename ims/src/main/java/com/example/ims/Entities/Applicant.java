b47a0e3672319668a0ada697e345c7afc6cb1075
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 09:53:29 +0400

    add relationship

--- ims/src/main/java/com/example/ims/Entities/Applicant.java
+++ ims/src/main/java/com/example/ims/Entities/Applicant.java
@@ -1,13 +1,20 @@
 package com.example.ims.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Applicant extends Person{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String passportNumber;
+    private String nationality;
+    private Boolean criminalRecord;
+    @OneToMany
+    List<VisaApplications> visaApplications;
+    @OneToMany
+    List<Interview> interviews;
+
 }
