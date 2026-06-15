dc6172b73d28102279aa03269ef73ec7879d1747
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 10:26:37 +0400

    add fields

--- ims/src/main/java/com/example/ims/Entities/Interview.java
+++ ims/src/main/java/com/example/ims/Entities/Interview.java
@@ -1,4 +1,23 @@
 package com.example.ims.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Interview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Applicant applicant;
+    @ManyToOne
+    private ImmigrationOfficer officer;
+    private String interviewDate;
+    private String status;
+    private String purpose;
 }
