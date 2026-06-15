53596de9450933a73ccef9b1df6dec8bc95073fd
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 10:08:45 +0400

    add fields

--- ims/src/main/java/com/example/ims/Entities/BorderControllerOfficer.java
+++ ims/src/main/java/com/example/ims/Entities/BorderControllerOfficer.java
@@ -1,4 +1,10 @@
 package com.example.ims.Entities;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class BorderControllerOfficer  extends  ImmigrationOfficer{
+    private String assignedCheckpoint;
+    private  Boolean k9UnitAssigned;
+
 }
