fe6b388b430c1b7a043caeb66a066cb803a1c9a9
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 10:19:55 +0400

    add fields

--- ims/src/main/java/com/example/ims/Entities/ImmigrationCenter.java
+++ ims/src/main/java/com/example/ims/Entities/ImmigrationCenter.java
@@ -1,4 +1,19 @@
 package com.example.ims.Entities;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class ImmigrationCenter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String locationCountry;
+    private String type;
+    private int dailyCapacity;
+    @OneToMany
+    private List<ImmigrationOfficer> officers;
+
 }
