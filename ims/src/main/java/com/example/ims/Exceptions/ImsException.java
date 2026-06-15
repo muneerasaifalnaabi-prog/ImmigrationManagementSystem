948957f18f14c21d338a19c9f15c7cff76ec5ec8
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 14:42:10 +0400

    add notFound exception case

--- ims/src/main/java/com/example/ims/Exceptions/ImsException.java
+++ ims/src/main/java/com/example/ims/Exceptions/ImsException.java
@@ -1,7 +1,15 @@
 package com.example.ims.Exceptions;
 
+import org.springframework.http.HttpStatus;
+
 public class ImsException extends RuntimeException {
-    public ImsException(String message) {
+    private  HttpStatus status;
+
+    public ImsException(HttpStatus status, String message) {
         super(message);
+        this.status = status;
+    }
+    public static ImsException notFound(String message) {
+        return new ImsException(HttpStatus.NOT_FOUND, message);
     }
 }
