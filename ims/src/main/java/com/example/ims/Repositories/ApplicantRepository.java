6289ce24dee248164b3fa08bd66878814bd96a6f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 10:30:41 +0400

    add OfficerRepository

--- ims/src/main/java/com/example/ims/Repositories/ApplicantRepository.java
+++ ims/src/main/java/com/example/ims/Repositories/ApplicantRepository.java
@@ -2,6 +2,8 @@ package com.example.ims.Repositories;
 
 import com.example.ims.Entities.Applicant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ApplicantRepository extends JpaRepository<Applicant,Long> {
 }
--- ims/src/main/java/com/example/ims/Repositories/OfficerRepository.java
+++ ims/src/main/java/com/example/ims/Repositories/OfficerRepository.java
@@ -2,6 +2,8 @@ package com.example.ims.Repositories;
 
 import com.example.ims.Entities.ImmigrationOfficer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OfficerRepository extends JpaRepository<ImmigrationOfficer,Long> {
 }
