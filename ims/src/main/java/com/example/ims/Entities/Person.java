b64d1316ba5a7dcc4646fc162fe233843d101ddd
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 09:31:37 +0400

    add fields

--- ims/src/main/java/com/example/ims/Entities/Person.java
+++ ims/src/main/java/com/example/ims/Entities/Person.java
@@ -1,4 +1,15 @@
 package com.example.ims.Entities;
 
-public class Person {
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+
+abstract class Person {
+    private String  firstName;
+    private String lastName;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+
+
 }
