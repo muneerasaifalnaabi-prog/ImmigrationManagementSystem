14902ed35c22ac4a300c9bfc373569fc73e242b0
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 09:32:32 +0400

    add lombok annotation for getter & setter

--- ims/src/main/java/com/example/ims/Entities/Person.java
+++ ims/src/main/java/com/example/ims/Entities/Person.java
@@ -1,9 +1,14 @@
 package com.example.ims.Entities;
 
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @MappedSuperclass
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 abstract class Person {
     private String  firstName;
     private String lastName;
