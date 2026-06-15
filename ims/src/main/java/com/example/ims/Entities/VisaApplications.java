2c0653f5c06c281915f25780fc9d41ad815463f9
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 10:24:48 +0400

    add fields

--- ims/src/main/java/com/example/ims/Entities/VisaApplications.java
+++ ims/src/main/java/com/example/ims/Entities/VisaApplications.java
@@ -1,19 +1,25 @@
 package com.example.ims.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ManyToAny;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VisaApplications {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToAny
+    @ManyToOne
     private Applicant applicant;
-
-
+    @ManyToOne
+    private ImmigrationOfficer handlingOfficer;
+    private String visaType;
+    private String status;
+    private String officerNotes;
 
 }
