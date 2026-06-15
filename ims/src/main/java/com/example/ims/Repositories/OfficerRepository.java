fa324ed1b7b9493eea37037e2c6cfcc56cb56b56
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 16:02:54 +0400

    add findOfficersByRankAndClearanceLevel  method

--- ims/src/main/java/com/example/ims/Repositories/OfficerRepository.java
+++ ims/src/main/java/com/example/ims/Repositories/OfficerRepository.java
@@ -2,6 +2,8 @@ package com.example.ims.Repositories;
 
 import com.example.ims.Entities.ImmigrationOfficer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +16,10 @@ public interface OfficerRepository extends JpaRepository<ImmigrationOfficer, Lon
 
     List<ImmigrationOfficer> findByActive(boolean active);
     List<ImmigrationOfficer> findByCenterId(Long centerId);
+
+    @Query(" SELECT o FROM ImmigrationOfficer o WHERE o.ranks = :rank AND o.clearancelevel >= :minimumClearanceLevel")
+    List<ImmigrationOfficer> findOfficersByRankAndClearanceLevel(@Param("rank") String rank, @Param("minimumClearanceLevel") int minimumClearanceLevel);
+}
 
 
 }
