06b4dc3ee5b6c89ab061c194757788ce9a72b520
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 16:27:08 +0400

    add submitApplication  method

--- ims/src/main/java/com/example/ims/Services/VisaApplicationService.java
+++ ims/src/main/java/com/example/ims/Services/VisaApplicationService.java
@@ -1,4 +1,39 @@
 package com.example.ims.Services;
 
+import com.example.ims.Entities.Applicant;
+import com.example.ims.Entities.VisaApplications;
+import com.example.ims.Exceptions.ImsException;
+import com.example.ims.Repositories.ApplicantRepository;
+import com.example.ims.Repositories.OfficerRepository;
+import com.example.ims.Repositories.VisaApplicationRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class VisaApplicationService {
+    @Autowired
+    VisaApplicationRepository visaApplicationRepository;
+    @Autowired
+    ApplicantRepository applicantRepository;
+    @Autowired
+    OfficerRepository officerRepository;
+
+    public VisaApplications submitApplication(Long applicantId, String visaType) {
+        Applicant applicant = applicantRepository.findById(applicantId).orElseThrow(() -> ImsException.notFound("Applicant Not Found"));
+        VisaApplications visa = new VisaApplications();
+
+        visa.setApplicant(applicant);
+        visa.setVisaType(visaType);
+
+        if (applicant.getCriminalRecord() == true) {
+            visa.setStatus("REJECTED");
+            visa.setOfficerNotes("Auto-rejected due to criminal flag.");
+        } else {
+            visa.setStatus("PENDING");
+        }
+        return visaApplicationRepository.save(visa);
+    }
+
+
 }
