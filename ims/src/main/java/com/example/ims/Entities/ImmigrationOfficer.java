8130036d02e09239a8c07c24f79fa0c07194fcb7
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 10:01:12 +0400

    add id as primary key

--- ims/src/main/java/com/example/ims/Entities/ImmigrationOfficer.java
+++ ims/src/main/java/com/example/ims/Entities/ImmigrationOfficer.java
@@ -1,4 +1,14 @@
 package com.example.ims.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class ImmigrationOfficer extends Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
