d75aac776d3a809b878463e5fc651bb5804dcf9e
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 10:14:10 +0400

    add id as primary key

--- ims/src/main/java/com/example/ims/Entities/BorderControllerOfficer.java
+++ ims/src/main/java/com/example/ims/Entities/BorderControllerOfficer.java
@@ -1,8 +1,14 @@
 package com.example.ims.Entities;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BorderControllerOfficer  extends  ImmigrationOfficer{
     private String assignedCheckpoint;
     private  Boolean k9UnitAssigned;
--- ims/src/main/java/com/example/ims/Entities/VisaApplications.java
+++ ims/src/main/java/com/example/ims/Entities/VisaApplications.java
@@ -1,4 +1,15 @@
 package com.example.ims.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class VisaApplications {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
 }
