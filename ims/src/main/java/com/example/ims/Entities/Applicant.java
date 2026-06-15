8a60093f500f11df3858837ca086ea870e53e643
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 09:36:09 +0400

    add id field as primary key

--- ims/src/main/java/com/example/ims/Entities/Applicant.java
+++ ims/src/main/java/com/example/ims/Entities/Applicant.java
@@ -1,7 +1,13 @@
 package com.example.ims.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Applicant extends Person{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
