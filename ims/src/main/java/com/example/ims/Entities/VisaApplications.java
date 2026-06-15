31ac587524ea5ad9f7d20c5cb80a61e9572ef98c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-15 10:17:43 +0400

    add ImmigrationCenter  class

--- ims/src/main/java/com/example/ims/Entities/VisaApplications.java
+++ ims/src/main/java/com/example/ims/Entities/VisaApplications.java
@@ -4,12 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.ManyToAny;
 
 @Entity
 public class VisaApplications {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToAny
+    private Applicant applicant;
+
 
 
 }
