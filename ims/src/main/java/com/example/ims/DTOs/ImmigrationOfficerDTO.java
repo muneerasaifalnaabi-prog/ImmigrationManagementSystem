de365f3d65f2cd526e9f8c2bb945435609dbc816
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 10:44:05 +0400

    add ImmigrationOfficerDTO class with fields

---
+++ ims/src/main/java/com/example/ims/DTOs/ImmigrationOfficerDTO.java
@@ -0,0 +1,13 @@
+package com.example.ims.DTOs;
+
+
+public class ImmigrationOfficerDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String badgeNumber;
+    private String ranks;
+    private Boolean active;
+
+
+}
