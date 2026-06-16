f789a8a70582d3d4879754c9a244466fe13fd678
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 10:48:27 +0400

    add convertToDTO method

--- ims/src/main/java/com/example/ims/DTOs/ImmigrationOfficerDTO.java
+++ ims/src/main/java/com/example/ims/DTOs/ImmigrationOfficerDTO.java
@@ -11,14 +11,25 @@ public class ImmigrationOfficerDTO {
     private Long id;
     private String firstName;
     private String lastName;
-    private String badgeNumber;
     private String ranks;
     private Boolean active;
+    private String centerName;
 
-    public static  ImmigrationOfficerDTO convertToDTO(ImmigrationOfficer entity){
+    public static ImmigrationOfficerDTO convertToDTO(ImmigrationOfficer entity) {
         ImmigrationOfficerDTO dto = new ImmigrationOfficerDTO();
 
+        dto.setId(entity.getId());
+        dto.setFirstName(entity.getFirstName());
+        dto.setLastName(entity.getLastName());
+        dto.setRanks(entity.getRanks());
+        dto.setActive(entity.getActive());
+
+        if (entity.getCenter() != null) {
+            dto.setCenterName(entity.getCenter().getName());
+        }
+        return dto;
     }
+
 
 
 }
