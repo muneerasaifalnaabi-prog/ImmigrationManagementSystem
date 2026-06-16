8cc94041ea0d03f5017b3bc487b5d9b0e2f42a78
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 08:48:07 +0400

    add processVisa method

--- ims/src/main/java/com/example/ims/Services/VisaApplicationService.java
+++ ims/src/main/java/com/example/ims/Services/VisaApplicationService.java
@@ -6,6 +6,7 @@ import com.example.ims.Exceptions.ImsException;
 import com.example.ims.Repositories.ApplicantRepository;
 import com.example.ims.Repositories.OfficerRepository;
 import com.example.ims.Repositories.VisaApplicationRepository;
+import jakarta.validation.ValidationException;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,17 @@ public class VisaApplicationService {
         } else {
             visa.setStatus("PENDING");
         }
+        return visaApplicationRepository.save(visa);
+    }
+
+    public VisaApplications processVisa(Long visaId, String newStatus, String notes) {
+
+        VisaApplications visa = visaApplicationRepository.findById(visaId).orElseThrow(() -> ImsException.notFound("Visa application not found"));
+        if (!newStatus.equalsIgnoreCase("APPROVED") && !newStatus.equalsIgnoreCase("REJECTED")) {
+            throw ImsException.badRequest("Status must be APPROVED or REJECTED");
+        }
+        visa.setStatus(newStatus);
+        visa.setOfficerNotes(notes);
         return visaApplicationRepository.save(visa);
     }
 
