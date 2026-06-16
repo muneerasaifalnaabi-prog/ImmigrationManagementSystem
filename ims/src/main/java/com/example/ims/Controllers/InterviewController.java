3c699713b77cca2bf4f7890c8d80aa051e3fd312
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 23:19:52 +0400

    Add api endpoint for scheduleInterview

--- ims/src/main/java/com/example/ims/Controllers/InterviewController.java
+++ ims/src/main/java/com/example/ims/Controllers/InterviewController.java
@@ -1,7 +1,21 @@
 package com.example.ims.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ims.Entities.Interview;
+import com.example.ims.Services.InterviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("interviews")
 public class InterviewController {
+    @Autowired
+    InterviewService interviewService;
+
+    @PostMapping("/schedule/{applicantId}/{officerId}")
+    public ResponseEntity<Interview> scheduleInterview(@PathVariable Long applicantId, @PathVariable Long officerId, @RequestParam String date) {
+
+        return ResponseEntity.ok(interviewService.scheduleInterview(applicantId, officerId, date));
+
+    }
 }
