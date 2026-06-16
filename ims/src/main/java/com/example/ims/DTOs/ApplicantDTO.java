697fa3ebfb72759b90545c2395d597450693b93a
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 10:32:44 +0400

    add ApplicantDTO method

--- ims/src/main/java/com/example/ims/DTOs/ApplicantDTO.java
+++ ims/src/main/java/com/example/ims/DTOs/ApplicantDTO.java
@@ -1,11 +1,14 @@
 package com.example.ims.DTOs;
 
 
+import com.example.ims.Entities.Applicant;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ApplicantDTO {
     private Long id;
     private String firstName;
@@ -13,4 +16,11 @@ public class ApplicantDTO {
     private String passportNumber;
     private String nationality;
     private Boolean criminalRecord;
+
+    public static ApplicantDTO  convertToDTO(Applicant entity){
+        ApplicantDTO dto = new ApplicantDTO();
+        dto.setId(entity.getId());
+
+    }
 }
+
