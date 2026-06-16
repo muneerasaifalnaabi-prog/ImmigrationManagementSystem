02ec29d0ada4406e8234dfaa5f429b1ce1659878
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 09:15:45 +0400

    add scheduleInterview method

--- ims/src/main/java/com/example/ims/Services/InterviewService.java
+++ ims/src/main/java/com/example/ims/Services/InterviewService.java
@@ -1,5 +1,9 @@
 package com.example.ims.Services;
 
+import com.example.ims.Entities.Applicant;
+import com.example.ims.Entities.ImmigrationOfficer;
+import com.example.ims.Entities.Interview;
+import com.example.ims.Exceptions.ImsException;
 import com.example.ims.Repositories.ApplicantRepository;
 import com.example.ims.Repositories.CenterRepository;
 import com.example.ims.Repositories.InterviewRepository;
@@ -7,15 +11,27 @@ import com.example.ims.Repositories.OfficerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class InterviewService {
     @Autowired
     InterviewRepository interviewRepository;
     @Autowired
     ApplicantRepository applicantRepository;
-
     @Autowired
-    OfficerRepository officerRepository
+    OfficerRepository officerRepository;
+
+    public Interview scheduleInterview(Long applicantId, Long officerId, String date) {
+        Applicant applicant = applicantRepository.findById(applicantId).orElseThrow(() -> ImsException.notFound("Applicant not found"));
+        ImmigrationOfficer officer = officerRepository.findById(officerId).orElseThrow(() -> ImsException.notFound("Officer not found"));
+        List<Interview> existingInterviews = interviewRepository.findByOfficerIdAndInterviewDate(officerId, date);
+        if (!existingInterviews.isEmpty()) {
+            throw ImsException.badRequest("Officer is double-booked!");
+        }
+        Interview interview = new Interview();
+
+    }
 
 
 
