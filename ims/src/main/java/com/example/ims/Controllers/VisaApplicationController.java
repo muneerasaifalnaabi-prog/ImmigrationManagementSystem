babd658d0067ee79559090afd283775db1d84bcf
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 15:32:41 +0400

    add endpoint for submitVisa

--- ims/src/main/java/com/example/ims/Controllers/VisaApplicationController.java
+++ ims/src/main/java/com/example/ims/Controllers/VisaApplicationController.java
@@ -1,13 +1,21 @@
 package com.example.ims.Controllers;
 
+import com.example.ims.Entities.VisaApplications;
 import com.example.ims.Services.VisaApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("visas")
 public class VisaApplicationController {
     @Autowired
     VisaApplicationService visaApplicationService;
+
+    @PostMapping("/submit/{applicantId}")
+    public VisaApplications submitVisa(@PathVariable Long applicantId, @RequestParam String type){
+        return visaApplicationService.submitApplication(applicantId, type);
+    }
+
 
 
 }
