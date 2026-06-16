46820076200f43fac3eb568b58b344c2fd5743bd
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 23:50:36 +0400

    Add DatabaseSeedController  class

--- ims/src/main/java/com/example/ims/Controllers/DatabaseSeedController.java
+++ ims/src/main/java/com/example/ims/Controllers/DatabaseSeedController.java
@@ -1,7 +1,21 @@
 package com.example.ims.Controllers;
 
+import com.example.ims.Repositories.ApplicantRepository;
+import com.example.ims.Repositories.CenterRepository;
+import com.example.ims.Repositories.OfficerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api")
 public class DatabaseSeedController {
+    @Autowired
+    CenterRepository centerRepository;
+    @Autowired
+    OfficerRepository officerRepository;
+    @Autowired
+    ApplicantRepository applicantRepository;
+
+
 }
