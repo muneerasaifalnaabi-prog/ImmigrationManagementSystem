9a92331051c8971cb49efbcdb1a85c451a785d3f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 11:08:09 +0400

    add convertT method

--- ims/src/main/java/com/example/ims/DTOs/InterviewDTO.java
+++ ims/src/main/java/com/example/ims/DTOs/InterviewDTO.java
@@ -1,5 +1,6 @@
 package com.example.ims.DTOs;
 
+import com.example.ims.Entities.Interview;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,5 +11,16 @@ public class InterviewDTO {
     private String interviewDate;
     private String status;
     private String purpose;
+
+    public static InterviewDTO convertToDTO(Interview entity) {
+
+        InterviewDTO dto = new InterviewDTO();
+
+        dto.setId(entity.getId());
+        dto.setInterviewDate(entity.getInterviewDate());
+        dto.setStatus(entity.getStatus());
+        dto.setPurpose(entity.getPurpose());
+        return dto;
+    }
 
 }
