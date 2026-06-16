73b68a4817f1ed7903fc7e28953f47a7a276c8cb
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 14:24:39 +0400

    add controllers classes

--- ims/src/main/java/com/example/ims/Controllers/OfficersController.java
+++ ims/src/main/java/com/example/ims/Controllers/OfficersController.java
@@ -1,8 +1,12 @@
 package com.example.ims.Controllers;
 
+import com.example.ims.Services.OfficerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OfficersController {
+    @Autowired
+    OfficerService officerService;
 
 }
