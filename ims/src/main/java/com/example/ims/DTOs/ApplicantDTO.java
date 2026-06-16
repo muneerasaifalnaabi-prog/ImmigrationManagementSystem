b331dc66c87b795ea93644ff1e1e3214f1ec36fe
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 10:30:10 +0400

    add fields

--- ims/src/main/java/com/example/ims/DTOs/ApplicantDTO.java
+++ ims/src/main/java/com/example/ims/DTOs/ApplicantDTO.java
@@ -1,4 +1,16 @@
 package com.example.ims.DTOs;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class ApplicantDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String passportNumber;
+    private String nationality;
+    private Boolean criminalRecord;
 }
