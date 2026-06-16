e0b117c43beefef3ae35ebdb61df1765f0255516
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 13:01:34 +0400

    add addApplicant method

--- ims/src/main/java/com/example/ims/Controllers/ApplicantsController.java
+++ ims/src/main/java/com/example/ims/Controllers/ApplicantsController.java
@@ -1,7 +1,11 @@
 package com.example.ims.Controllers;
 
+import com.example.ims.DTOs.ApplicantDTO;
 import com.example.ims.Services.ApplicantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplicantsController {
     @Autowired
     ApplicantService  applicantService;
+
+    @PostMapping("/add")
+    public ResponseEntity<ApplicantDTO> addApplicant(@RequestBody ApplicantDTO applicantDTO) {
+
+    }
 }
