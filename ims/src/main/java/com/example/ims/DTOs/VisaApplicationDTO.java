41856d75839e57e2e5081d1e9535943dc695a65b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 10:56:00 +0400

    add VisaApplicationDTO with fields

--- ims/src/main/java/com/example/ims/DTOs/VisaApplicationDTO.java
+++ ims/src/main/java/com/example/ims/DTOs/VisaApplicationDTO.java
@@ -1,4 +1,13 @@
 package com.example.ims.DTOs;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class VisaApplicationDTO {
+    private Long id;
+    private String visaType;
+    private String status;
+
 }
