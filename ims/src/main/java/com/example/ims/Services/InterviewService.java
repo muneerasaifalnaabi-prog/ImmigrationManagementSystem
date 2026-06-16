08f3186e8319f53bf516838e3d84d12326bdd39c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 09:11:43 +0400

    add objects

--- ims/src/main/java/com/example/ims/Services/InterviewService.java
+++ ims/src/main/java/com/example/ims/Services/InterviewService.java
@@ -1,13 +1,21 @@
 package com.example.ims.Services;
 
+import com.example.ims.Repositories.ApplicantRepository;
 import com.example.ims.Repositories.CenterRepository;
+import com.example.ims.Repositories.InterviewRepository;
 import com.example.ims.Repositories.OfficerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InterviewService {
+    @Autowired
+    InterviewRepository interviewRepository;
+    @Autowired
+    ApplicantRepository applicantRepository;
 
+    @Autowired
+    OfficerRepository officerRepository
 
 
 
