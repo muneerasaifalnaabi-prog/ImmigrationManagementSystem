978eb06011cd960e058f416a8c807218dc32b45b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 11:02:26 +0400

    add convertT method

--- ims/src/main/java/com/example/ims/DTOs/VisaApplicationDTO.java
+++ ims/src/main/java/com/example/ims/DTOs/VisaApplicationDTO.java
@@ -1,8 +1,12 @@
 package com.example.ims.DTOs;
 
+import com.example.ims.Entities.ImmigrationOfficer;
 import com.example.ims.Entities.VisaApplications;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -11,13 +15,23 @@ public class VisaApplicationDTO {
     private String visaType;
     private String status;
 
-    public static VisaApplicationDTO convertToDTO(VisaApplications entity){
+    public static VisaApplicationDTO convertToDTO(VisaApplications entity) {
         VisaApplicationDTO dto = new VisaApplicationDTO();
         dto.setId(entity.getId());
         dto.setVisaType(entity.getVisaType());
         dto.setStatus(entity.getStatus());
-
+        return dto;
     }
 
+    public static List<VisaApplicationDTO> convertToDTO(List<VisaApplications> entities) {
 
+        List<VisaApplicationDTO> dtos = new ArrayList<>();
+        for (VisaApplications entity : entities) {
+            dtos.add(convertToDTO(entity));
+        }
+        return dtos;
+    }
 }
+
+
+
