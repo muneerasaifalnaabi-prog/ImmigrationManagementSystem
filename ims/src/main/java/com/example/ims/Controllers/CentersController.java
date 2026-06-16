aa33ba2ff1b9283aa9fd9da9bf87f9b39f9be85b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 11:28:51 +0400

    add CenterService class

--- ims/src/main/java/com/example/ims/Controllers/CentersController.java
+++ ims/src/main/java/com/example/ims/Controllers/CentersController.java
@@ -1,8 +1,13 @@
 package com.example.ims.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("centers")
 public class CentersController {
+
+
 
 }
---
+++ ims/src/main/java/com/example/ims/Services/CenterService.java
@@ -0,0 +1,4 @@
+package com.example.ims.Services;
+
+public class CenterService {
+}
