4dc27d1e5075286f5b2cbde96fe86a97ba1a982a
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 11:36:44 +0400

    add fields

--- ims/src/main/java/com/example/ims/DTOs/CenterDTO.java
+++ ims/src/main/java/com/example/ims/DTOs/CenterDTO.java
@@ -1,4 +1,11 @@
 package com.example.ims.DTOs;
 
 public class CenterDTO {
+    private Long id;
+    private String name;
+    private String locationCountry;
+    private String type;
+    private int dailyCapacity;
+
+
 }
