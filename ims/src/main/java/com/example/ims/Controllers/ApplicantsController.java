11210c5173ccfed31c8d5da50d360de261de934c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 13:00:14 +0400

    add ApplicantsController class

--- ims/src/main/java/com/example/ims/Controllers/ApplicantsController.java
+++ ims/src/main/java/com/example/ims/Controllers/ApplicantsController.java
@@ -1,4 +1,9 @@
 package com.example.ims.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("applicants")
 public class ApplicantsController {
 }
