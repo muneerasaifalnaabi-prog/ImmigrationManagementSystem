76ad69c1d6f685c8131da924c29c84c361224b78
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 13:00:41 +0400

    add ApplicantsController class

--- ims/src/main/java/com/example/ims/Controllers/ApplicantsController.java
+++ ims/src/main/java/com/example/ims/Controllers/ApplicantsController.java
@@ -1,9 +1,13 @@
 package com.example.ims.Controllers;
 
+import com.example.ims.Services.ApplicantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("applicants")
 public class ApplicantsController {
+    @Autowired
+    ApplicantService  applicantService;
 }
