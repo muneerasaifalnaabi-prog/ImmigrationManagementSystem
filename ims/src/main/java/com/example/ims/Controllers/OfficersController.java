42cd38da8b37b372a97fdeaf149c3e0b7f643f1c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-16 14:24:00 +0400

    add controllers classes

--- ims/src/main/java/com/example/ims/Controllers/OfficersController.java
+++ ims/src/main/java/com/example/ims/Controllers/OfficersController.java
@@ -1,5 +1,8 @@
 package com.example.ims.Controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OfficersController {
 
 }
--- ims/src/main/java/com/example/ims/Controllers/VisaApplicationController.java
+++ ims/src/main/java/com/example/ims/Controllers/VisaApplicationController.java
@@ -1,4 +1,13 @@
 package com.example.ims.Controllers;
 
+import com.example.ims.Services.VisaApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class VisaApplicationController {
+    @Autowired
+    VisaApplicationService visaApplicationService;
+
+
 }
