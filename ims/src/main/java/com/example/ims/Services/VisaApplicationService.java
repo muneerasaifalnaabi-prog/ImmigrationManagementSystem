89497d4ba9dd662340692ec79ba6e70e11f2c5c7
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-17 09:33:36 +0400

    delete unnecessary repository methods

--- ims/src/main/java/com/example/ims/Services/VisaApplicationService.java
+++ ims/src/main/java/com/example/ims/Services/VisaApplicationService.java
@@ -9,7 +9,6 @@ import com.example.ims.Repositories.ApplicantRepository;
 import com.example.ims.Repositories.OfficerRepository;
 import com.example.ims.Repositories.VisaApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
